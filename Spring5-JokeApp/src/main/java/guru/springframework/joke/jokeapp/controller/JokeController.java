ac1cb5ecccdfa57dda3e2b9f6a29abefd0c03e6b
David Jiang <djiang86@binghamton.edu>
2018-03-20 12:52:14 +0000

    controller and service layer

---
+++ Spring5-JokeApp/src/main/java/guru/springframework/joke/jokeapp/controller/JokeController.java
@@ -0,0 +1,39 @@
+package guru.springframework.joke.jokeapp.controller;
+
+import guru.springframework.joke.jokeapp.services.JokeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+
+    private JokeService jokeService;
+
+    @Autowired
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model){
+
+        model.addAttribute("jokes", jokeService.getJoke());
+
+        return "chucknorris";
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
