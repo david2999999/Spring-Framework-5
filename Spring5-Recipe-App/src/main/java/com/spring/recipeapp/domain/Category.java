a280614e090b769e1d66ffe02ddd32c7c6d118dc
David Jiang <djiang86@binghamton.edu>
2018-03-26 12:26:19 +0000

    @ManyToMany relationship and @JoinTable

---
+++ Spring5-Recipe-App/src/main/java/com/spring/recipeapp/domain/Category.java
@@ -0,0 +1,41 @@
+package com.spring.recipeapp.domain;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String description;
+
+    @ManyToMany(mappedBy = "categories")
+    private Set<Recipe> recipes;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Set<Recipe> getRecipes() {
+        return recipes;
+    }
+
+    public void setRecipes(Set<Recipe> recipes) {
+        this.recipes = recipes;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
