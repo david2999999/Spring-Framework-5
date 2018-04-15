508589cc3fbe87f4c4bbcd2b5b303f202af3d334
David Jiang <djiang86@binghamton.edu>
2018-04-15 15:14:17 +0000

    redirect to 404error.html when the page is not found

--- Spring5-Recipe-Application/src/main/java/com/spring/controllers/RecipeController.java
+++ Spring5-Recipe-Application/src/main/java/com/spring/controllers/RecipeController.java
@@ -1,11 +1,14 @@
 package com.spring.controllers;
 
+import com.spring.exceptions.NotFoundException;
 import com.spring.services.RecipeService;
 import com.spring.commands.RecipeCommand;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 
 // Controller that will be in charge of all of the CRUD routing methods on the recipe
@@ -74,5 +77,18 @@ public class RecipeController {
 
         recipeService.deleteById(Long.valueOf(id));
         return "redirect:/";
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    public ModelAndView handleNotFound(){
+
+        log.error("Handling not found exception");
+
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.setViewName("404error");
+
+        return modelAndView;
     }
 }
--- Spring5-Recipe-Application/src/test/java/com/spring/controllers/RecipeControllerTest.java
+++ Spring5-Recipe-Application/src/test/java/com/spring/controllers/RecipeControllerTest.java
@@ -73,8 +73,10 @@ public class RecipeControllerTest {
         when(recipeService.findById(anyLong())).thenThrow(NotFoundException.class);
 
         mockMvc.perform(get("/recipe/1/show"))
-                .andExpect(status().isNotFound());
+                .andExpect(status().isNotFound())
+                .andExpect(view().name("404error"));
     }
+
 
     @Test
     public void testGetNewRecipeForm() throws Exception {
