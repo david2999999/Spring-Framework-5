fdb956b9b7b9baaebb6bc234df1d9f2999996b82
David Jiang <djiang86@binghamton.edu>
2018-04-16 03:09:10 +0000

    field validation with javax validation constraints

--- Spring5-Recipe-Application/src/main/java/com/spring/controllers/RecipeController.java
+++ Spring5-Recipe-Application/src/main/java/com/spring/controllers/RecipeController.java
@@ -7,14 +7,19 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
 
 
 // Controller that will be in charge of all of the CRUD routing methods on the recipe
 @Slf4j
 @Controller
 public class RecipeController {
+
+    private static final String RECIPE_RECIPEFORM_URL = "recipe/recipeform";
 
     // create a recipeService that has all of the CRUD methods
     private final RecipeService recipeService;
@@ -48,7 +53,7 @@ public class RecipeController {
 
     // route to show the form to update a recipe
     @GetMapping("recipe/{id}/update")
-    public String updateRecipe(@PathVariable String id, Model model){
+    public String updateRecipe(@PathVariable String id, Model model) {
         // find the command object that will be used to bind to the form
         // the form will be populated with the data of the command object
         // inside the recipeService method, the original domain object will be
@@ -56,16 +61,24 @@ public class RecipeController {
         model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(id)));
 
         // the thymeleaf view page to update a recipe
-        return "recipe/recipeform";
+        return RECIPE_RECIPEFORM_URL;
     }
 
     // Post route to save or update a recipe
     @PostMapping("recipe")
-    public String saveOrUpdate(@ModelAttribute RecipeCommand command){
-        // save the recipe and return the command object that was saved
+    public String saveOrUpdate(@Valid @ModelAttribute("recipe") RecipeCommand command, BindingResult bindingResult){
+
+        if(bindingResult.hasErrors()){
+
+            bindingResult.getAllErrors().forEach(objectError -> {
+                log.debug(objectError.toString());
+            });
+
+            return RECIPE_RECIPEFORM_URL;
+        }
+
         RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
 
-        // redirect the user to show the page of the recipe that was saved or updated
         return "redirect:/recipe/" + savedCommand.getId() + "/show";
     }
 
@@ -89,21 +102,6 @@ public class RecipeController {
         ModelAndView modelAndView = new ModelAndView();
 
         modelAndView.setViewName("404error");
-        modelAndView.addObject("exception", exception);
-
-        return modelAndView;
-    }
-
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(NumberFormatException.class)
-    public ModelAndView handleNumberFormat(Exception exception){
-
-        log.error("Handling Number Format Exception");
-        log.error(exception.getMessage());
-
-        ModelAndView modelAndView = new ModelAndView();
-
-        modelAndView.setViewName("400error");
         modelAndView.addObject("exception", exception);
 
         return modelAndView;
