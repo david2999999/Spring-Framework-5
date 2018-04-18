a6f7f7bca2d408fb21eb9b250580701c12897102
David Jiang <djiang86@binghamton.edu>
2018-04-18 05:02:44 +0000

    default, dev, prod profiles

--- Spring5-Recipe-Application/src/main/java/com/spring/bootstrap/RecipeBootstrap.java
+++ Spring5-Recipe-Application/src/main/java/com/spring/bootstrap/RecipeBootstrap.java
@@ -6,6 +6,7 @@ import com.spring.repositories.RecipeRepository;
 import com.spring.repositories.UnitOfMeasureRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,6 +19,7 @@ import java.util.Optional;
 // This bootstrap class will create 2 recipes and save the 2 recipe to the H2 database
 @Slf4j
 @Component
+@Profile("default")
 public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEvent> {
 
     private final CategoryRepository categoryRepository;
